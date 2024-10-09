e3debd68183fa983e0ab54918125bbe54fbc5112
Rafael Diniz <82606617+Kayler-Rafa@users.noreply.github.com>
2024-10-09 20:04:05 -0300

    New branch

--- src/app.js
+++ src/app.js
@@ -1,13 +1,9 @@
-const express = require('express');
+import express from 'express';
+
 const app = express();
 
 app.get('/', (req, res) => {
-    res.status(200).send('Hello World!');
+  res.send('Hello World!');
 });
 
-const port = process.env.PORT || 3000;
-app.listen(port, () => {
-    console.log(`Server running on port ${port}`);
-});
-
-module.exports = app;
+export { app };
--- test/app.test.js
+++ test/app.test.js
@@ -1,17 +1,18 @@
-const chai = require('chai');
-const chaiHttp = require('chai-http');
-const app = require('../src/app');
-chai.should();
+import chai from 'chai';
+import chaiHttp from 'chai-http';
+import { app } from '../src/app.js';  // Supondo que você tenha um arquivo app.js que exporta a aplicação Express
+
+const { expect } = chai;
 chai.use(chaiHttp);
 
-describe("GET /", () => {
-    it("should return Hello World!", (done) => {
-        chai.request(app)
-            .get('/')
-            .end((err, res) => {
-                res.should.have.status(200);
-                res.text.should.be.eql('Hello World!');
-                done();
-            });
-    });
+describe('GET /', () => {
+  it('should return Hello World!', (done) => {
+    chai.request(app)
+      .get('/')
+      .end((err, res) => {
+        expect(res).to.have.status(200);
+        expect(res.text).to.equal('Hello World!');
+        done();
+      });
+  });
 });
